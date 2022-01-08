4743b649b4b6bdc9cded9930a07c31829a8273ac
smori <shinichiro.mori.1983@gmail.com>
2022-01-09 01:29:05 +0900

    dummy pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,27 @@ module.exports = {
   head: [],
   temp: 'docs/.vuepress/.temp',
   dest: 'docs/.vuepress/dist',
+
+  themeConfig: {
+    sidebar: [
+      {
+        title: '国',
+        collapsable: false,
+        children: [
+          '/country/india.md',
+          '/country/nepal.md',
+          '/country/sri_lanka.md',
+        ],
+      },
+      {
+        title: '構文解析',
+        collapsable: false,
+        children: [
+          '/parsing/parse_tree.md',
+          '/parsing/parser_generator.md',
+          '/parsing/lexical_analysis.md',
+        ],
+      }
+    ],
+  },
 };
