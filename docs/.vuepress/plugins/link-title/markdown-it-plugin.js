e93ddffbb44575799b0df00c73bb87a3e7724afa
smori <shinichiro.mori.1983@gmail.com>
2022-01-27 18:05:50 +0900

    Inline isSimpleLink()

--- docs/.vuepress/plugins/link-title/markdown-it-plugin.js
+++ docs/.vuepress/plugins/link-title/markdown-it-plugin.js
@@ -7,7 +7,12 @@ const modifyLinkTitle = (pages, tokens, marker) => {
   tokens.forEach((token) => {
     if (token.type === 'inline' && token.children.length > 0) {
       for (let i = 0, len = token.children.length; i < len; i++) {
-        if (isSimpleLink(token.children, i)) {
+        if (
+          token.children.length > (i + 2) &&
+          token.children[i].type === 'link_open' &&
+          token.children[i + 1].type === 'text' &&
+          token.children[i + 2].type === 'link_close'
+        ) {
           const href = token.children[i].attrGet('href');
 
           let page;
@@ -21,20 +26,6 @@ const modifyLinkTitle = (pages, tokens, marker) => {
       }
     }
   });
-};
-
-/**
- * @param {Object[]} tokens
- * @param {number} index
- * @returns {boolean}
- */
-const isSimpleLink = (tokens, index) => {
-  return (
-    tokens.length > (index + 2) &&
-    tokens[index + 0].type === 'link_open' &&
-    tokens[index + 1].type === 'text' &&
-    tokens[index + 2].type === 'link_close'
-  );
 };
 
 const findPageForHref = (pages, href) => {
