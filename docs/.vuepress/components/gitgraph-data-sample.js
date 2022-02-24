c4fecd8bbf1f2646ee382e5c6776e59025a4fcf4
smori <shinichiro.mori.1983@gmail.com>
2022-02-25 03:11:15 +0900

    Format gitgraph-data-sample.js

--- docs/.vuepress/components/gitgraph-data-sample.js
+++ docs/.vuepress/components/gitgraph-data-sample.js
@@ -1,45 +1,45 @@
 export default {
-  "defaultBranch": "master",
-  "actions": [
+  defaultBranch: 'master',
+  actions: [
     {
-      "type": "commit",
-      "branch": "master",
-      "message": "initial commit"
+      type: 'commit',
+      branch: 'master',
+      message: 'initial commit',
     },
     {
-      "type": "branch:create",
-      "branch": "feature/1",
-      "from": "master"
+      type: 'branch:create',
+      branch: 'feature/1',
+      from: 'master',
     },
     {
-      "type": "branch:create",
-      "branch": "feature/2",
-      "from": "master"
+      type: 'branch:create',
+      branch: 'feature/2',
+      from: 'master',
     },
     {
-      "type": "commit",
-      "branch": "feature/1",
-      "message": "1"
+      type: 'commit',
+      branch: 'feature/1',
+      message: '1',
     },
     {
-      "type": "commit",
-      "branch": "feature/2",
-      "message": "2"
+      type: 'commit',
+      branch: 'feature/2',
+      message: '2',
     },
     {
-      "type": "commit",
-      "branch": "feature/1",
-      "message": "3"
+      type: 'commit',
+      branch: 'feature/1',
+      message: '3',
     },
     {
-      "type": "commit",
-      "branch": "feature/2",
-      "message": "4"
+      type: 'commit',
+      branch: 'feature/2',
+      message: '4',
     },
     {
-      "type": "merge",
-      "branch": "feature/1",
-      "into": "master"
-    }
-  ]
+      type: 'merge',
+      branch: 'feature/1',
+      into: 'master',
+    },
+  ],
 };
