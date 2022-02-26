d7eb882e34ea4fcfe5f08acffdeb56df19e6a879
smori <shinichiro.mori.1983@gmail.com>
2022-02-26 17:16:54 +0900

    Display AST

--- docs/.vuepress/components/PlaygroundGitGraphEditor.vue
+++ docs/.vuepress/components/PlaygroundGitGraphEditor.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <div class="container-editor">
+    <div class="box container-editor">
+      <div class="container-title">Input</div>
       <textarea
         v-model="input"
         @keyup="render()"
       ></textarea>
     </div>
-    <div class="container-graph">
-      <div ref="graph"></div>
+    <div class="box container-result">
+      <div class="container-graph">
+        <div class="container-title">Graph (SVG)</div>
+        <div ref="graph"></div>
+      </div>
+      <div class="container-ast">
+        <div class="container-title">AST</div>
+        <pre class="ast">{{ ast }}</pre>
+      </div>
     </div>
   </div>
 </template>
@@ -44,6 +52,7 @@ export default {
         'git merge feature/1\n' +
         'git tag v1.0.0\n',
       graph: null,
+      ast: '',
     };
   },
 
@@ -83,9 +92,11 @@ export default {
       const logger = new Format1GitLogger();
 
       try {
-        parser.parse(this.input);
+        const parsed = parser.parse(this.input);
 
         this.graph.clear();
+
+        this.ast = JSON.stringify(parsed, null, 2);
 
         logger.create(this.graph, this.input);
       } catch (e) {
@@ -97,16 +108,43 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.container-editor {
-  margin-bottom 2rem
+$gitgraphFontSize = 15px
 
+.box {
+  margin-bottom 2rem
+}
+
+.container-title {
+  font-weight bold
+  margin-bottom 1rem
+}
+
+.container-editor {
   textarea {
     display block
     margin 0
     min-width 100%
     min-height 150px
+    font-size $gitgraphFontSize
   }
 }
-.container-graph {
+.container-result {
+  display flex
+
+  .container-graph {
+    width 60%
+  }
+  .container-ast {
+    width 40%
+
+    .ast {
+      margin 0
+      padding 0.5rem
+      border 1px solid gray
+      background-color white
+      color black
+      font-size $gitgraphFontSize
+    }
+  }
 }
 </style>
