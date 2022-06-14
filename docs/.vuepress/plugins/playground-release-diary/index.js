48ccdb277c7b775cc393499c3b0417d73da4c59e
smori <shinichiro.mori.1983@gmail.com>
2022-06-14 22:29:43 +0900

    Initial implement of additionalPages

--- docs/.vuepress/plugins/playground-release-diary/index.js
+++ docs/.vuepress/plugins/playground-release-diary/index.js
@@ -1,5 +1,88 @@
+/**
+ * @typedef {import('vuepress-types').Context} Context
+ * @typedef {import('vuepress-types').Page} Page
+ * @typedef {import('vuepress-types').PluginOptionAPI} PluginOptionAPI
+ */
+
+const sprintf = require('sprintf-js').sprintf;
+const PackageContainer = require('./package-container');
+
+/**
+ * @param {Object} options
+ * @param {Context} ctx
+ * @return {PluginOptionAPI}
+ */
 module.exports = (options, ctx) => {
+  const pathPrefix = 'release';
+
+  const prepareDiaryPages = async () => {
+    const result = [];
+
+    const container = new PackageContainer();
+    ctx.pages.filter(isTargetPage).forEach((page) => {
+      const {date, name, version} = page.frontmatter.package_release;
+      const path = page.regularPath;
+      container.add(date, name, version, path);
+    });
+
+    const indexPageLines = [];
+    indexPageLines.push('# Release date list');
+    indexPageLines.push('');
+    container.getDateList().forEach((date) => {
+      indexPageLines.push(sprintf(
+        '- [%s](/%s/%s/)',
+        date,
+        pathPrefix,
+        date.replace(/\//g, '')
+      ));
+    });
+
+    result.push({
+      path: sprintf('/%s/', pathPrefix),
+      content: indexPageLines.join('\n'),
+    });
+
+    container.getDateList().forEach((date) => {
+      const dateDigits = date.replace(/\//g, '');
+
+      const datePageLines = [];
+      datePageLines.push(sprintf('# Release list (%s)', date));
+      datePageLines.push('');
+      container.getByDate(date).forEach((pkg) => {
+        datePageLines.push(sprintf(
+          '- [%s (%s)](%s)',
+          pkg.name,
+          pkg.version,
+          pkg.path
+        ));
+      });
+
+      result.push({
+        path: sprintf('/%s/%s/', pathPrefix, dateDigits),
+        content: datePageLines.join('\n'),
+      })
+    });
+
+    return result;
+  };
+
+  /**
+   * @param {Page} page
+   * @return {boolean}
+   */
+  const isTargetPage = (page) => {
+    return (
+      page.frontmatter.package_release &&
+      page.frontmatter.package_release.date &&
+      page.frontmatter.package_release.name &&
+      page.frontmatter.package_release.version
+    );
+  }
+
   return {
     name: 'playground-release-diary',
+    async additionalPages() {
+      return await prepareDiaryPages();
+    }
   };
 };
---
+++ docs/.vuepress/plugins/playground-release-diary/package-container.js
@@ -0,0 +1,68 @@
+const PackageInfo = require('./package-info');
+
+class PackageContainer {
+  constructor() {
+    /**
+     * @type {PackageInfo[]}
+     * @private
+     */
+    this._packages = [];
+  }
+
+  /**
+   * @param {string} date
+   * @param {string} name
+   * @param {string} version
+   * @param {string} path
+   */
+  add(date, name, version, path) {
+    this._packages.push(new PackageInfo(date, name, version, path));
+  }
+
+  /**
+   * @return {string[]}
+   */
+  getDateList() {
+    /**
+     * @type {Set<string>}
+     */
+    const dates = new Set();
+
+    this._packages.forEach((pkg) => {
+      dates.add(pkg.date);
+    });
+
+    const result = Array.from(dates);
+
+    result.sort((a, b) => {
+      return a <= b ? 1 : -1;
+    });
+
+    return result;
+  }
+
+  /**
+   * @param {string} date
+   * @return {PackageInfo[]}
+   */
+  getByDate(date) {
+    /**
+     * @type {PackageInfo[]}
+     */
+    const result = [];
+
+    this._packages.forEach((pkg) => {
+      if (pkg.date === date) {
+        result.push(pkg);
+      }
+    });
+
+    result.sort((a, b) => {
+      return a.name <= b.name ? -1 : 1;
+    })
+
+    return result;
+  }
+}
+
+module.exports = PackageContainer;
