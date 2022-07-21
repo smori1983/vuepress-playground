404c6e9956b65256f2fba5f6b4b5299e049e95da
smori <shinichiro.mori.1983@gmail.com>
2022-07-21 22:51:04 +0900

    Add debug component

--- docs/.vuepress/plugins/playground-update-info/enhanceAppFile.js
+++ docs/.vuepress/plugins/playground-update-info/enhanceAppFile.js
@@ -1,5 +1,7 @@
 import PlaygroundUpdateInfoList from './components/PlaygroundUpdateInfoList';
+import PlaygroundUpdateInfoDebug from './components/PlaygroundUpdateInfoDebug';
 
 export default ({Vue}) => {
   Vue.component('PlaygroundUpdateInfoList', PlaygroundUpdateInfoList);
+  Vue.component('PlaygroundUpdateInfoDebug', PlaygroundUpdateInfoDebug);
 }
