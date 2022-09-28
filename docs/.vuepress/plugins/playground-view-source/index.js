37c8826432f3383ae5e787bf791505960d0ee1e1
smori <shinichiro.mori.1983@gmail.com>
2022-09-28 22:16:41 +0900

    Option for marker notations

--- docs/.vuepress/plugins/playground-view-source/index.js
+++ docs/.vuepress/plugins/playground-view-source/index.js
@@ -5,14 +5,22 @@
 const path = require('path');
 
 /**
+ * @param {Object} options
  * @return {PluginOptionAPI}
  */
-module.exports = () => ({
-  name: 'playground-view-source',
-  enhanceAppFiles: [
-    path.resolve(__dirname, 'enhanceAppFile.js'),
-  ],
-  extendMarkdown: (md) => {
-    md.use(require('./markdown-it-plugin'));
-  },
-});
+module.exports = (options) => {
+  const {
+    markerDefault = '[[source]]',
+    markerContainer = '[[source:container]]',
+  } = options;
+
+  return {
+    name: 'playground-view-source',
+    enhanceAppFiles: [
+      path.resolve(__dirname, 'enhanceAppFile.js'),
+    ],
+    extendMarkdown: (md) => {
+      md.use(require('./markdown-it-plugin')(markerDefault, markerContainer));
+    },
+  };
+};
--- docs/.vuepress/plugins/playground-view-source/markdown-it-plugin.js
+++ docs/.vuepress/plugins/playground-view-source/markdown-it-plugin.js
@@ -1,35 +1,44 @@
 const { Base64 } = require('js-base64');
 
-const markerDefault = '[[source]]';
-const markerContainer = '[[source:container]]';
-
-module.exports = (md) => {
-  md.block.ruler.before('paragraph', 'playground_view_source', blockRule);
+/**
+ * @param {string} markerDefault
+ * @param {string} markerContainer
+ */
+module.exports = (markerDefault, markerContainer) => {
+  return (md) => {
+    md.block.ruler.before('paragraph', 'playground_view_source', blockRule(markerDefault, markerContainer));
+  };
 };
 
-const blockRule = (state, startLine, endLine, silent) => {
-  const lineText = state.src.slice(state.bMarks[startLine], state.eMarks[startLine]);
+/**
+ * @param {string} markerDefault
+ * @param {string} markerContainer
+ */
+const blockRule = (markerDefault, markerContainer) => {
+  return (state, startLine, endLine, silent) => {
+    const lineText = state.src.slice(state.bMarks[startLine], state.eMarks[startLine]);
 
-  let display = null;
+    let display = null;
 
-  if (lineText === markerDefault) {
-    display = 'default';
-  } else if (lineText === markerContainer) {
-    display = 'container';
-  } else {
-    return false;
-  }
+    if (lineText === markerDefault) {
+      display = 'default';
+    } else if (lineText === markerContainer) {
+      display = 'container';
+    } else {
+      return false;
+    }
 
-  state.line = startLine + 1;
+    state.line = startLine + 1;
 
-  const encoded = Base64.encode(state.src);
+    const encoded = Base64.encode(state.src);
 
-  const token = new state.Token('html_block', '', 0);
-  token.map = [startLine, state.line];
-  token.content = `<PlaygroundViewSourceDefault display="${display}">${encoded}</PlaygroundViewSourceDefault>`;
-  token.block = true;
+    const token = new state.Token('html_block', '', 0);
+    token.map = [startLine, state.line];
+    token.content = `<PlaygroundViewSourceDefault display="${display}">${encoded}</PlaygroundViewSourceDefault>`;
+    token.block = true;
 
-  state.tokens.push(token);
+    state.tokens.push(token);
 
-  return true;
+    return true;
+  };
 };
